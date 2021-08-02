e5f8645611157e0b696b003703e42a91301c72b9
Maksim Iakunin <yakuninm+github@gmail.com>
2021-08-02 15:54:58 +0300

    Init commit

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gocarina/gocsv"
+)
+
+type WpPost struct {
+	Id        string `csv:"id"`
+	Title     string `csv:"Title"`
+	Permalink string `csv:"Permalink"`
+	Status    string `csv:"Status"`
+}
+
+func main() {
+	path := os.Args[1]
+	posts := getWpPosts(path)
+
+	for i, post := range posts {
+		if post.Status == "publish" {
+			fmt.Printf(
+				"(%d of %d) Processing url = %s \n",
+				i,
+				len(posts),
+				post.Permalink,
+			)
+
+			processUrl(post.Permalink)
+		}
+	}
+}
+
+func getWpPosts(path string) []*WpPost {
+	in, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(in *os.File) {
+		_ = in.Close()
+	}(in)
+
+	var posts []*WpPost
+
+	if err := gocsv.UnmarshalFile(in, &posts); err != nil {
+		panic(err)
+	}
+
+	return posts
+}
+
+func processUrl(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		cacheStatus := resp.Header.Get("CF-Cache-Status")
+		fmt.Println("CF-Cache-Status = ", cacheStatus)
+		if cacheStatus == "HIT" {
+			break
+		}
+	}
+}
