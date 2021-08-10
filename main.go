14ffcb5fcf435de0d8bbec2b2e314fb6b17551fb
Maksim Iakunin <yakuninm+github@gmail.com>
2021-08-10 15:50:32 +0300

    Reading from stdin instead of absolute filepath

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/gocarina/gocsv"
+	"io"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/gocarina/gocsv"
 )
 
-type WpPost struct {
+type Item struct {
 	Id        string `csv:"id"`
 	Title     string `csv:"Title"`
 	Permalink string `csv:"Permalink"`
@@ -17,41 +17,30 @@ type WpPost struct {
 }
 
 func main() {
-	path := os.Args[1]
-	posts := getWpPosts(path)
-
-	for i, post := range posts {
-		if post.Status == "publish" {
+	items := getItems(os.Stdin)
+	for i, item := range items {
+		if item.Status == "publish" {
 			fmt.Printf(
 				"(%d of %d) Processing url = %s \n",
 				i,
-				len(posts),
-				post.Permalink,
+				len(items),
+				item.Permalink,
 			)
 
-			processUrl(post.Permalink)
+			processUrl(item.Permalink)
 		}
 	}
 }
 
-func getWpPosts(path string) []*WpPost {
-	in, err := os.Open(path)
+func getItems(reader io.Reader) []*Item {
+	var items []*Item
+
+	err := gocsv.Unmarshal(reader, &items)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer func(in *os.File) {
-		_ = in.Close()
-	}(in)
-
-	var posts []*WpPost
-
-	err = gocsv.UnmarshalFile(in, &posts)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return posts
+	return items
 }
 
 func processUrl(url string) {
